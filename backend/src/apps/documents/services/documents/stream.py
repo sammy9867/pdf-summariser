397ce30ad5073b9751c107ccbc7d521f4a2e8613
Samuel Menezes <samuelmenezes15@gmail.com>
2025-08-02 22:34:07 +0100

    update stream functionality

--- backend/src/apps/documents/services/documents/stream.py
+++ backend/src/apps/documents/services/documents/stream.py
@@ -1,25 +1,7 @@
-import os
 import time
 from typing import Generator
 
 from apps.documents.models import Document
-from apps.documents.services.exceptions import DocumentSummaryStreamError
-
-
-def document_can_stream_summary(
-    document: Document, raise_exception: bool = True
-) -> bool:
-    codes = DocumentSummaryStreamError.Code
-    try:
-        file_path = document.uploaded_file.file.path
-
-        if not os.path.exists(file_path):
-            raise DocumentSummaryStreamError(codes.INVALID_FILE_PATH)
-    except DocumentSummaryStreamError as e:
-        if raise_exception:
-            raise e
-        return False
-    return True
 
 
 def document_stream_summary(document: Document) -> Generator[str, None, None]:
--- backend/src/tests/apps/documents/services/documents/test_stream.py
+++ backend/src/tests/apps/documents/services/documents/test_stream.py
@@ -1,38 +1,20 @@
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import pytest
 
 from apps.documents.factories import DocumentFactory
-from apps.documents.services.documents.stream import (
-    DocumentSummaryStreamError,
-    document_can_stream_summary,
-    document_stream_summary,
-)
+from apps.documents.services.documents.stream import document_stream_summary
 
 pytestmark = pytest.mark.django_db
 
 
-@pytest.fixture
-def document():
-    return DocumentFactory()
-
-
-class TestDocumentCanStreamSummary:
-    @patch("os.path.exists", new=Mock(return_value=True))
-    def test__can_stream__success(self, document):
-        assert document_can_stream_summary(document)
-
-    @patch("os.path.exists", new=Mock(return_value=False))
-    def test__can_stream__fails(self, document):
-        with pytest.raises(DocumentSummaryStreamError) as exc_info:
-            document_can_stream_summary(document)
-
-        assert exc_info.value.code == DocumentSummaryStreamError.Code.INVALID_FILE_PATH
-
-
 class TestDocumentStreamSummary:
+    @pytest.fixture
+    def document(self):
+        return DocumentFactory()
+
     @patch("time.sleep")
-    def test__stream__success(self, document):
+    def test_stream__success(self, document):
         summary = document_stream_summary(document)
         results = list(summary)
         combined_text = "".join(results)
